f75fc180b1f17c64b357abbebbee27ddc01dd3f4
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-04-03 21:08:00 -0500

    added config.fish

---
+++ config.fish
@@ -0,0 +1,13 @@
+# config.fish
+
+export PATH="$PATH:/home/pi/.local/bin"
+
+# my aliases
+
+alias py="python3"
+# alias cat="bat"
+alias vim="nvim"
+alias ls="exa -lFmh --group-directories-first"
+alias la="exa -alFmh --group-directories-first"
+
+starship init fish | source
