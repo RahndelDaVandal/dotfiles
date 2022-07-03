6a455d8ddcfc75189fc43900f0ba003ecc01373b
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-07-03 00:02:06 -0500

    updated config.fish

--- config.fish
+++ config.fish
@@ -7,7 +7,8 @@ export PATH="$PATH:/home/pi/.local/bin"
 alias py="python3"
 # alias cat="bat"
 alias vim="nvim"
-alias ls="exa -lFmh --group-directories-first"
-alias la="exa -alFmh --group-directories-first"
+# alias ls="exa -lFmh --group-directories-first"
+alias ls="exa -alFmh --group-directories-first"
+# alias la="exa -alFmh --group-directories-first"
 
 starship init fish | source
