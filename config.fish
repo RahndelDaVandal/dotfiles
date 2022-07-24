cf30e96576de3df88a670bef85eaa4be2a4854f6
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-07-24 12:49:21 -0500

    tweaked some things in starship and added some aliases in config.fish. Need to add some validation to make sure the packages are installed and if not then not alias

--- config.fish
+++ config.fish
@@ -1,14 +1,33 @@
 # config.fish
+# set fish_greeting ""
 
-export PATH="$PATH:/home/pi/.local/bin"
+fish_add_path -a /home/pi/.local/bin
+fish_add_path -a /usr/local/go/bin
+fish_add_path -a ~/.cargo/bin
+
+# export PATH="$PATH:/home/pi/.local/bin"
+# export PATH="$PATH:/usr/local/go/bin"
+# export PATH="$PATH:~/.cargo/bin"
+
+zoxide init fish | source
 
 # my aliases
 
-alias py="python3"
-# alias cat="bat"
+alias cp="cp -i"
+alias rm="rm -i"
+alias mv="mv -i"
+alias pytest="py -m pytest"
+alias py="python3.10"
+alias cat="bat"
 alias vim="nvim"
-# alias ls="exa -lFmh --group-directories-first"
 alias ls="exa -alFmh --group-directories-first"
-# alias la="exa -alFmh --group-directories-first"
+alias find="fd"
+alias grep="rg"
+alias du="dust"
+# alias time="hyperfine"
+alias ps="procs"
+alias sed="sd"
+alias top="btm"
+alias htop="btm"
 
 starship init fish | source
