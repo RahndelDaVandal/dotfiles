dd858b8f014c982204198ea88a9e5ebe503a854f
Connor Sahleen <Connor.Sahleen@gmail.com>
2025-02-06 11:06:04 -0600

    Initial commit of new bare repo. Added dotfiles for bash, tmux, and fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,38 @@
+# config.fish
+
+set -g fish_greeting
+
+fish_add_path -a /home/$USER/.local/bin
+fish_add_path -a ~/.cargo/bin
+fish_add_path -a /home/$USER/miniconda3/bin
+fish_add_path -a /usr/local/go/bin
+
+set -g BROWSER /usr/bin/browser_in_win
+set -g NVM_DIR $HOME/.nvm
+# set -g nvm_default_version '18.15.0'
+
+# my aliases
+alias py="python3"
+alias cat="bat"
+alias vim="nvim"
+alias ls="exa -alFmh --group-directories-first"
+alias ps="powershell.exe -Command"
+alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+function t
+    if tmux ls &>/dev/null
+        tmux attach
+    else
+        tmux
+    end
+end
+
+starship init fish | source
+zoxide init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/connor/miniconda3/bin/conda
+    eval /home/connor/miniconda3/bin/conda "shell.fish" hook $argv | source
+end
+# <<< conda initialize <<<
---
+++ .config/fish/functions/deploy.fish
@@ -0,0 +1,33 @@
+function deploy
+    if test (count $argv) -ne 1
+        echo "Usage: deploy <target>"
+        return 1
+    end
+
+    set target $argv[1]
+
+    # Prepare $temp_dir on $target
+    ssh admin@$target '[ -d /home/admin/.tmp_deploy ] && rm -rf /home/admin/.tmp_deploy/* || mkdir -p /home/admin/.tmp_deploy;'
+
+    if test $status -ne 0
+        echo "Error preparing temp directory on target, aborting deploy...."
+        return 1
+    end
+
+    # Copy ipk(s) to /home/admin/.tmp_deploy on $target
+    scp -r *.ipk admin@$target:/home/admin/.tmp_deploy
+
+    if test $status -ne 0
+        echo "Error copying ipk(s) to target, aborting deploy...."
+        return 1
+    end
+
+    # Install ipk(s) on $target
+    ssh admin@$target 'opkg update && opkg install /home/admin/.tmp_deploy/*.ipk && reboot || exit 1'
+
+    if test $status -ne 0
+        echo "Error installing ipk(s) on target, aborting deploy...."
+        return 1
+    end
+
+end
