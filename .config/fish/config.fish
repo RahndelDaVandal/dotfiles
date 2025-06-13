4f59d3c9e1daca2725e9eb7218314d32c3879f99
Connor Sahleen <Connor.Sahleen@gmail.com>
2025-06-13 14:12:58 -0500

    added usb alias to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ fish_add_path -a /home/$USER/.local/bin
 fish_add_path -a ~/.cargo/bin
 fish_add_path -a /home/$USER/miniconda3/bin
 fish_add_path -a /usr/local/go/bin
+fish_add_path -a $HOME/go/bin
 
 set -g BROWSER /usr/bin/browser_in_win
 set -g NVM_DIR $HOME/.nvm
@@ -16,8 +17,11 @@ alias py="python3"
 alias cat="bat"
 alias vim="nvim"
 alias ls="exa -alFmh --group-directories-first"
-alias ps="powershell.exe -Command"
 alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+alias pwrs="powershell.exe -Command"
+alias list-usb='powershell.exe -Command "usbipd list"'
+alias attach-usb='powershell.exe -Command "usbipd attach --wsl --busid"'
+alias detach-usb='powershell.exe -Command "usbipd detach --busid"'
 
 function t
     if tmux ls &>/dev/null
